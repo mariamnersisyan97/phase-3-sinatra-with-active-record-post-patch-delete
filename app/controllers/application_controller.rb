453a2ec0856e500d957a0da6712028feae909154
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-08-08 13:21:43 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,28 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  delete '/reviews/:id' do
+    game = Review.find(params[:id])
+    game.destroy
+    game.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
 end
